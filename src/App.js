0c82fd65c13832433621715a26df208ce1921177
Ian Band <ian.r.band@gmail.com>
2020-03-03 17:13:24 -0800

    stripped old boilerplate

--- src/App.js
+++ src/App.js
@@ -5,17 +5,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-// COMPONENTS //
-import { Route, Redirect, Switch, NavLink} from 'react-router-dom';
-
-// VIEWS/COMPOONENTS //
-import Post from './components/Post.js';
-import Cube from './components/Cube.js';
-
-import CollatzDemo from './components/CollatzDemo.js';
-import OEISDemo from './components/OEISDemo.js';
-
-import content from './content.js';
 
 class App extends Component{
     constructor(props){
@@ -24,66 +13,7 @@ class App extends Component{
     render(){
         return (
             <div>
-
-                {/* nav */}
-                <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-expand">
-                    <NavLink className={"navbar-brand"} to={'/'}>Ian Band</NavLink>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <a className={"nav-item nav-link"} href={`https://github.com/IanBand?tab=repositories`}>Github</a>
-                            <a className={"nav-item nav-link"} href={`https://www.linkedin.com/in/ian-band-b10b58181/`}>Linkedin</a>
-                            <a className={"nav-item nav-link"} href={`http://web.engr.oregonstate.edu/~bandi/resume.pdf`}>Resume</a>
-                        </div>
-                    </div>
-                    <div></div>
-                </nav>
-
-
-                {/* routes */}
-                <Switch>
-                    <Route path="/" exact render={(route) => window.scrollTo(0, 0)}/>
-                    
-
-                    {content.map((item, i) =>(
-                            <Route path={`/${item.route}`} exact render={() => {
-                                setTimeout(() => {
-                                    let elem = document.getElementById(`item-${i}`);
-                                    if(elem) elem.scrollIntoView({ behavior: 'smooth', block: 'center' });
-                                }, 169);
-
-                            }}
-                            key={i}
-                            /> 
-                    ))}
-
-                    <Route render={() => <Redirect to={'/'}/>}/> {/*order of routes matters, catch all has to be last :P */}
-                </Switch>
-
-
-                {/* content */}
-                {content.map((item, i) => (
-                            item.type == 'post' ?
-                            <Post
-                                key={i}
-                                title={item.title}
-                                link={item.link}
-                                text={item.text}
-                                route={item.route}
-                                images={item.images}
-                                id={`item-${i}`}
-
-                            />
-                            :
-                            <h1 key={i} id={`item-${i}`} className={'container p3'}>
-                                {item.title}
-                            </h1>
-                ))}
-
-                <Cube/>
-
+                sup
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@
 // REACT //
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter } from 'react-router-dom'
 
 // REDUX //
 import { Provider } from 'react-redux';
 import store from './redux/store/index.js';
-
-// BOOTSTRAP //
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// CUBE CSS //
-import './assets/cube.css'
 
 // APP //
 import App from './App.js';
@@ -20,38 +13,7 @@ import App from './App.js';
 
 render( 
     <Provider store={store}>
-        <HashRouter>
-            <App/>
-        </HashRouter>
+        <App/>
     </Provider>,
     document.getElementById('app')
 );
-
-
-
-//cube scroll rotate logic
-const getDocHeight = () =>{
-    let D = document;
-    return Math.max(
-        D.body.scrollHeight, D.documentElement.scrollHeight,
-        D.body.offsetHeight, D.documentElement.offsetHeight,
-        D.body.clientHeight, D.documentElement.clientHeight
-    )
-}
-
-let totalFrames = 1000;
-const amountscrolled = () => {
-    let winheight= window.innerHeight || (document.documentElement || document.body).clientHeight;
-    let docheight = getDocHeight();
-    let scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    let trackLength = docheight - winheight;
-    let pctScrolled = Math.floor(scrollTop/trackLength * totalFrames); // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
-    //console.log(pctScrolled);
-    return pctScrolled;
-}
-
-document.documentElement.style.setProperty('--total-frames', `${totalFrames}s`);
-window.onscroll = () =>{
-            //document.documentElement.style.setProperty('--cube-keyframe', `-${amountscrolled()}s`);
-            document.documentElement.style.setProperty('--cube-keyframe', `-${amountscrolled()}s`);
-}
