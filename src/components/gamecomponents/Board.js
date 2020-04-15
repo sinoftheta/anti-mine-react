f7180e9b944827c6ed1cfc308f45de6cd351c1f9
Ian Band <ian.r.band@gmail.com>
2020-04-14 23:48:23 -0700

    board random radius animation done

--- src/components/gamecomponents/Board.js
+++ src/components/gamecomponents/Board.js
@@ -8,11 +8,53 @@ import { bindActionCreators } from 'redux';
 import {colorMap} from '../../js/ColorMap.js';
 
 
+
+function animationTimings(x, y, width, height/*, centerFirst*/){
+    // calculate when and how fast to animate in
+    // tiles more central to the board will enter FIRST (or last, depending on centerFirst)
+
+    // total animation length in ms (duration + delay = totalDuration)
+    const totalDuration = 500;
+
+    const deviation = 0.35;
+    
+    let delay = Math.min(totalDuration, Math.floor( // cap value at totalDuration & floor
+
+                ((((Math.random() * 2 - 1) * deviation) + 1 ) // rand in [-dev, dev] + 1
+                *
+                Math.sqrt(Math.pow((x - width / 2 ), 2) + Math.pow((y - height / 2), 2))) // distance of tile from center
+                /
+                Math.max(width, height) // normalize by radius of field
+                *
+                totalDuration)) //multiply by totalDuration
+                ;
+
+    return {delay: delay, duration: totalDuration - delay};
+}
+
+
+const randTransformDir = () => {
+    // `translateY( -1.5 * calc(vars(----cell-size) * ${h}))`,
+
+    const vals = [
+        `translateX(100vw)`,
+        `translateX(-100vw)`,
+        `translateY(100vh)`,
+        `translateY(-100vh)`,
+    ];
+    return vals[Math.floor(Math.random() * 4)];
+}
+
+
 import './Board.css';
 // antiMine will be reset by its parent component when its settings prop changes
 class Board extends Component{
     constructor(props){
         super(props);
+
+        this.state={
+            anim: false,
+        }
     }
     shouldComponentUpdate(){
         console.log('board deciding to update!')
@@ -42,22 +84,42 @@ class Board extends Component{
 
                     //console.log(n)
                     //console.log(this.props.raster[n])
+
+                    let timings = animationTimings(i,j,this.props.board.length, this.props.board[0].length);
+                    
                     
                     return (
+                        
                         <div 
                             className={
-                                '__GAME__tile' + 
+                                '__GAME__tile ' + 
                                 (tile.revealed || this.props.renderUncovered ? 
-                                ' __GAME__tile-revealed' 
+                                '__GAME__tile-revealed ' 
                                 : 
-                                ' __GAME__tile-covered')
+                                '__GAME__tile-covered ')
+                                /*+
+                                (!this.props.animateIn ?
+                                `__GAME__tile-animate-in`
+                                :
+                                ''
+                                )*/
+
                             }
                             key={j}
                             x={tile.x}
                             y={tile.y}
                             style={{
-                                ...tile.revealed || this.props.renderUncovered ? {background:this.props.raster[n]} : {},
-                                ...tile.isMine ? {color: '#F23B5D' } : {}
+                                //tile background
+                                ...tile.revealed || this.props.renderUncovered ? {background:this.props.raster[n], borderColor:this.props.raster[n] } : {},
+
+                                //text color if mine
+                                ...tile.isMine ? {color: '#F23B5D' } : {},
+
+                                // WORKS, BUT NEEDS TO ONLY BE ACTIVE WHILE ANIMATING
+                                //animation properties
+                                transitionDelay: timings.delay + 'ms',
+                                transitionDuration: timings.duration + 'ms',
+                                transform: this.state.anim ? randTransformDir() : null,
                             }}
                             onClick={() => this.props.click(tile.x, tile.y)}
                         >
@@ -70,11 +132,18 @@ class Board extends Component{
                         </div>
                 )})}
                 </div>
+
+                
             );
             return (
+            <>
             <div id={'__GAME__board'}>
                 {tiles}
             </div>
+            <button
+            onClick={() => this.setState(p => ({anim: !p.anim}))}
+            >fuck</button>
+            </>
         );
     }
 }
